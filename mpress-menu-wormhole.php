5c8548f36ea59763eec78855e2ef9eb751de50e0
David Wood <dfwood90@gmail.com>
2017-01-20 12:39:29 -0500

    Updated in preparation to pass 10UP review.

--- mpress-menu-wormhole.php
+++ mpress-menu-wormhole.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Plugin Name: mPress Menu Wormhole
  * Plugin URI: https://wpscholar.com/wordpress-plugins/mpress-menu-wormhole/
@@ -40,7 +39,7 @@ class mPress_Menu_Wormhole {
 
 	function admin_init() {
 		global $pagenow;
-		if ( 'nav-menus.php' == $pagenow ) {
+		if ( 'nav-menus.php' === $pagenow ) {
 			$title = __( 'Navigation Menus', 'mpress-menu-wormhole' );
 			add_meta_box( 'taxonomy-nav_menu', $title, array( $this, 'show_metabox' ), 'nav-menus', 'side', 'low' );
 			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
@@ -71,20 +70,21 @@ class mPress_Menu_Wormhole {
 	public function do_ajax() {
 		if ( check_ajax_referer( self::$action, 'nonce' ) && current_user_can( 'edit_theme_options' ) ) {
 			require_once( ABSPATH . 'wp-admin/includes/nav-menu.php' );
-			if ( ! empty( $_POST['menus'] ) && is_array( $_POST['menus'] ) ) {
-				$item_ids = array();
+			$menu_ids = filter_input( INPUT_POST, 'menus', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY );
+			if ( ! empty( $menu_ids ) ) {
+				$item_ids   = array();
 				$menu_items = array();
-				foreach ( $_POST['menus'] as $menu_id ) {
+				foreach ( $menu_ids as $menu_id ) {
 					$menu_id = absint( $menu_id );
 					if ( is_nav_menu( $menu_id ) ) {
-						$menu = wp_get_nav_menu_object( $menu_id );
+						$menu           = wp_get_nav_menu_object( $menu_id );
 						$menu_item_data = array(
 							'menu-item-title'     => $menu->name,
 							'menu-item-type'      => 'taxonomy',
 							'menu-item-object'    => 'nav_menu',
 							'menu-item-object-id' => $menu->term_id,
 						);
-						$menu_item = wp_update_nav_menu_item( 0, 0, $menu_item_data );
+						$menu_item      = wp_update_nav_menu_item( 0, 0, $menu_item_data );
 						if ( ! is_wp_error( $menu_item ) ) {
 							$item_ids[] = $menu_item;
 						}
@@ -93,14 +93,14 @@ class mPress_Menu_Wormhole {
 				foreach ( $item_ids as $menu_item_id ) {
 					$menu_obj = get_post( $menu_item_id );
 					if ( ! empty( $menu_obj->ID ) ) {
-						$menu_obj = wp_setup_nav_menu_item( $menu_obj );
+						$menu_obj        = wp_setup_nav_menu_item( $menu_obj );
 						$menu_obj->label = $menu_obj->title; // don't show "(pending)" in ajax-added items
-						$menu_items[] = $menu_obj;
+						$menu_items[]    = $menu_obj;
 					}
 				}
 				if ( $menu_items ) {
 					$class = apply_filters( 'wp_edit_nav_menu_walker', 'Walker_Nav_Menu_Edit', 0 );
-					$args = array( 'walker' => new $class );
+					$args  = array( 'walker' => new $class );
 					echo walk_nav_menu_tree( $menu_items, 0, (object) $args );
 				}
 			}
@@ -111,7 +111,7 @@ class mPress_Menu_Wormhole {
 	}
 
 	function walker_nav_menu_start_el( $item_output, $item, $depth, $args ) {
-		if ( 'taxonomy' == $item->type && 'nav_menu' == $item->object ) {
+		if ( 'taxonomy' === $item->type && 'nav_menu' === $item->object ) {
 			if ( $menu = wp_get_nav_menu_object( $item->object_id ) ) {
 				global $wp_filter;
 				$filters = isset( $wp_filter['wp_nav_menu_args'] ) ? $wp_filter['wp_nav_menu_args'] : false;
